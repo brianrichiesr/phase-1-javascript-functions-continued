648ca624257f68014ee9ec60a8965ecab7b975b2
Brian Richie Sr <brianrichiesr@gmail.com>
2023-09-12 11:08:05 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = (task = "go to the office") => `This Monday, I will ${task}.`
+
+const wrapAdjective = (wrapper = "*") => {
+    return function highlighter(adj = "special") {
+        return `You are ${wrapper}${adj}${wrapper}!`;
+    }
+}
